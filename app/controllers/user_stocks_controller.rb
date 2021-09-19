d2ff9869752e4b378ac77e2c439d1a4ee668a2a4
shobhna1 <shobhnasinha@yahoo.com>
2021-09-18 20:39:27 -0700

    add stock tracking restrictions, remove tracking functionality, first and last name user model and modify views

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -3,7 +3,7 @@ class UserStocksController < ApplicationController
   def create
     stock = Stock.check_db(params[:ticker])
     if stock.blank?
-      stock = stock.new_lookup(params[:ticker])
+      stock = Stock.new_lookup(params[:ticker])
       stock.save
     end
     @user_stock = UserStock.create(user: current_user, stock: stock)
@@ -11,4 +11,11 @@ class UserStocksController < ApplicationController
     redirect_to my_portfolio_path
   end
 
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from tracking portfolio."
+    redirect_to my_portfolio_path
+  end
 end
