437eafbdc82bc94bbe5b70a92920598f027a9847
shobhna1 <shobhnasinha@yahoo.com>
2021-09-18 23:28:38 -0700

    add friendship self-referential association, my friends view with listing, and search friends form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
     @tracked_stocks = current_user.stocks
   end
 
+  def my_friends
+    @friend_list = current_user.friends
+  end
+
+  def search
+    render json: params[:friend]
+  end
+
 end
